4a1c74ebfbe3ba70c99702bcb19229ebf0fdf131
Kaito-Kido <maidailam2001@gmail.com>
2024-08-12 11:30:57 +0700

    update 920

--- solutions/920. Number of Music Playlists/Solution.java
+++ solutions/920. Number of Music Playlists/Solution.java
@@ -8,32 +8,32 @@
 
 public class Solution {
     final int MOD = 1000000007;
-    
+
     public int numMusicPlaylists(int n, int goal, int k) {
         // Memoization map
         Map<String, Integer> memo = new HashMap<>();
-        
+
         // Start the recursion with an empty playlist
         return recursion(n, goal, k, 0, new ArrayList<>(), memo);
     }
-    
+
     private int recursion(int n, int goal, int k, int index, List<Integer> queue, Map<String, Integer> memo) {
         Set<Integer> visited = new HashSet<>(queue);
-        
+
         if (index == goal) {
             return visited.size() == n ? 1 : 0;
         }
-        
+
         if (n - visited.size() > goal - index) {
             return 0;
         }
-        
+
         // Create a unique key for memoization
         String key = index + "-" + visited.size();
         if (memo.containsKey(key)) {
             return memo.get(key);
         }
-        
+
         int result = 0;
         for (int i = 1; i <= n; i++) {
             boolean found = false;
@@ -43,13 +43,14 @@ public class Solution {
                     break;
                 }
             }
-            if (found) continue;
+            if (found)
+                continue;
 
             queue.add(i);
             result = (result + recursion(n, goal, k, index + 1, queue, memo)) % MOD;
             queue.remove(queue.size() - 1);
         }
-        
+
         memo.put(key, result);
         return result;
     }
