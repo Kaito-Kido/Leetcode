5eb0d8aad0706f60da7f81db2a56d68fb9a765ec
Kaito-Kido <maidailam2001@gmail.com>
2024-08-16 10:47:42 +0700

    Update 154 to log(n)

--- solutions/154. Find Minimum in Rotated Sorted Array II/154.java
+++ solutions/154. Find Minimum in Rotated Sorted Array II/154.java
@@ -1,12 +1,42 @@
 // https://leetcode.com/problems/find-minimum-in-rotated-sorted-array-ii/
 
+// Solution: Binary Search
+
+// Complexity
+// Time: O(LogN)
+// Space: O(1)
+
 class Solution {
     public int findMin(int[] nums) {
-        int min = Integer.MAX_VALUE;
         int n = nums.length;
+        int first = nums[0];
+        int last = nums[n-1];
+        
+        if (last > first) {
+            return first;
+        }
 
-        for (int i = 0; i < n; i++) {
-            min = Math.min(min, nums[i]);
+        int left = 0;
+        int right = n-1;
+
+        return binarySearch(nums, left, right, first);
+    }
+
+    private int binarySearch(int[] nums, int left, int right, int first) {
+        int min = Integer.MAX_VALUE;
+
+        while (left <= right) {
+            int mid = left + ((right-left) / 2);
+
+            min = Math.min(nums[mid], min);
+            if (nums[mid] > first) {
+                left = mid + 1;
+            } else if ( nums[mid] < first ) {
+                right = mid - 1;
+            } else if (nums[mid] == first) {
+                int bothSideMin = Math.min(binarySearch(nums, left, mid - 1, first), binarySearch(nums, mid + 1, right, first));
+                return min = Math.min(min, bothSideMin);
+            }
         }
 
         return min;
