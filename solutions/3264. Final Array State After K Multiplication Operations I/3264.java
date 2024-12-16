dcf09ea017202d3c0ebf67efad8ed07b3448be36
Kaito-Kido <maidailam2001@gmail.com>
2024-12-16 10:09:47 +0700

    3264

---
+++ solutions/3264. Final Array State After K Multiplication Operations I/3264.java
@@ -0,0 +1,16 @@
+// TimeComplexity: O(Kn)
+
+class Solution {
+    public int[] getFinalState(int[] nums, int k, int multiplier) {
+        for (int i= 0; i < k; i++) {
+            int minIndex = 0;
+            for (int j = 0; j < nums.length; j++) {
+                if (nums[j] < nums[minIndex]) minIndex = j;
+            }
+
+            nums[minIndex] = nums[minIndex] * multiplier;
+        }
+        return nums;
+    }
+
+}
