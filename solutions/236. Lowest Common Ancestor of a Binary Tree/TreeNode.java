afed5f09ab9f8553f859d68323ca9e76f5746fc8
Kaito-Kido <maidailam2001@gmail.com>
2024-12-25 17:02:31 +0700

    236 unoptimized

---
+++ solutions/236. Lowest Common Ancestor of a Binary Tree/TreeNode.java
@@ -0,0 +1,63 @@
+import java.util.LinkedList;
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ * int val;
+ * TreeNode left;
+ * TreeNode right;
+ * TreeNode(int x) { val = x; }
+ * }
+ */
+
+public class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+
+    TreeNode(int x) {
+        val = x;
+    }
+}
+
+class Solution {
+    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+        LinkedList<TreeNode> ancestorListP = new LinkedList<>();
+        LinkedList<TreeNode> ancestorListQ = new LinkedList<>();
+
+        ancestorListP = findAncestorList(root, p, ancestorListP);
+        ancestorListQ = findAncestorList(root, q, ancestorListQ);
+
+        TreeNode commonA = null;
+
+        while (!ancestorListP.isEmpty() && !ancestorListQ.isEmpty()
+                && ancestorListP.getFirst().val == ancestorListQ.getFirst().val) {
+            commonA = ancestorListP.getFirst();
+            ancestorListP.removeFirst();
+            ancestorListQ.removeFirst();
+        }
+
+        return commonA;
+    }
+
+    private LinkedList<TreeNode> findAncestorList(TreeNode root, TreeNode value, LinkedList<TreeNode> ancestorList) {
+        ancestorList.add(root);
+
+        if (root.val == value.val) {
+            return ancestorList;
+        }
+
+        if (root.left != null) {
+            LinkedList<TreeNode> leftList = findAncestorList(root.left, value, ancestorList);
+            if (leftList.contains(value))
+                return leftList;
+        }
+        if (root.right != null) {
+            LinkedList<TreeNode> rightList = findAncestorList(root.right, value, ancestorList);
+            if (rightList.contains(value))
+                return rightList;
+        }
+        ancestorList.removeLast();
+        return ancestorList;
+    }
+}
