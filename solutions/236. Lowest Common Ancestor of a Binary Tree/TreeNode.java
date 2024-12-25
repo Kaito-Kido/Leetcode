877eab125796676217db8fd810847070879092fb
Kaito-Kido <maidailam2001@gmail.com>
2024-12-25 17:06:28 +0700

    236 optimize

--- solutions/236. Lowest Common Ancestor of a Binary Tree/TreeNode.java
+++ solutions/236. Lowest Common Ancestor of a Binary Tree/TreeNode.java
@@ -61,3 +61,21 @@ class Solution {
         return ancestorList;
     }
 }
+
+// Optimize Solution
+// class Solution {
+//     public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+//         if (root == null || root == p || root == q) {
+//             return root;
+//         }
+
+//         TreeNode left = lowestCommonAncestor(root.left, p, q);
+//         TreeNode right = lowestCommonAncestor(root.right, p, q);
+
+//         if (left != null && right != null) {
+//             return root;
+//         }
+
+//         return left != null ? left : right;
+//     }
+// }
